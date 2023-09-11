9d236c99beec2d9021fef3320a3e47bd0b6d8097
Yanzin <yantsantana155@gmail.com>
2023-09-11 15:45:03 -0300

    first commit

--- Tela2.py
+++ Tela2.py
@@ -2,8 +2,14 @@ from Modulos import *
 import customtkinter as Tk
 
 Tk.set_appearance_mode('Dark')
-Tk.set_default_color_theme('themes/dark-blue.json')
 
 Janela = CriarJanela('Janela Principal', '1500x700', 1, Redimensionar=False)
+
+LabelCamiseta = CriarLabel(Janela, 'Camiseta Blunt', 2, 6, 'White')
+LabelCamiseta.configure(font=('Arial', 17, 'bold'))
+
+ImagemCamiseta = CriarImagem(Janela, 'imagens/Camisa.jpg', 3, 6, 200, 200)
+
+
 
 Janela.mainloop()
